03a5c5e59646db08a7d791243cbe38df8eb4f538
katamilukova06 <122910788+katamilukova06@users.noreply.github.com>
2024-11-14 13:48:01 +0200

    add

---
+++ CulinaryBlog_milukovakatya-main/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using CulinaryBlog.Domain.ViewModels.LoginAndRegistration;
+using CulinaryBlog_milukovakatya.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CulinaryBlog_milukovakatya.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult SiteInformation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(model);
+            }  
+            var errors = ModelState.Values.SelectMany(v => v.Errors)
+                                           .Select(e => e.ErrorMessage)
+                                           .ToList();
+            return BadRequest(errors);
+        }
+        [HttpPost]
+        public IActionResult Register([FromBody] RegisterViewModel model) 
+        { 
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors)
+                                           .Select(e => e.ErrorMessage)
+                                           .ToList();
+                return BadRequest(errors);
+            }
+            return Ok(model);
+        }
+        [HttpPost]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CulinaryBlog_milukovakatya-main/wwwroot/js/login_and_registration_script.js
@@ -0,0 +1,140 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    function hiddenOpen_Closeclick() {
+        let x = document.querySelector(".container-login-registration");
+        if (x.style.display == "none") {
+            x.style.display = "grid";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    document.getElementById("click-to-hide").addEventListener("click", hiddenOpen_Closeclick);
+    document.querySelector(".overlay").addEventListener("click", hiddenOpen_Closeclick);
+    document.getElementById("side-menu-button-click-to-hide").addEventListener("click", hiddenOpen_Closeclick);
+
+    const signInBtn = document.querySelector('.signin-btn');
+    const signUpBtn = document.querySelector('.signup-btn');
+    const formBox = document.querySelector('.form-box');
+    const block = document.querySelector('.block');
+
+    if (signInBtn && signUpBtn) {
+        signUpBtn.addEventListener('click', function () {
+            formBox.classList.add('active');
+            block.classList.add('active');
+        });
+
+        signInBtn.addEventListener('click', function () {
+            formBox.classList.remove('active');
+            block.classList.remove('active');
+        });
+    }
+
+    const form_btn_signin = document.querySelector('.form_btn_signin');
+    const form_btn_signup = document.querySelector('.form_btn_signup');
+
+    if (form_btn_signin) {
+
+        form_btn_signin.addEventListener('click', function () {
+            const requestURL = '/Home/Login';
+
+            const errorContainer = document.getElementById('error-messages-singin');
+
+            const form = {
+                email: document.getElementById("signin_email"),
+                password: document.getElementById("signin_password")
+            }
+
+            const body = {
+                email: form.email.value,
+                password: form.password.value
+            }
+            sendRequest('POST', requestURL, body)
+                .then(data => {
+                    cleaningAndClosingForm(form, errorContainer);
+
+                    console.log('Успешный ответ:', data);
+
+                    location.reload();
+                })
+                .catch(err => {
+                    displayErrors(err, errorContainer);
+                    console.log(err);
+                });
+        });
+    }
+
+    if (form_btn_signup) {
+
+        form_btn_signup.addEventListener('click', function () {
+            const requestURL = '/Home/Register';
+
+            const errorContainer = document.getElementById('error-messages-singup');
+
+            const form = {
+                login: document.getElementById("signup_login"),
+                email: document.getElementById("signup_email"),
+                password: document.getElementById("signup_password"),
+                passwordConfirm: document.getElementById("signup_confirm_password"),
+            }
+            const body = {
+                login: form.login.value,
+                email: form.email.value,
+                password: form.password.value,
+                passwordConfirm: form.passwordConfirm.value,
+            }
+
+            sendRequest('POST', requestURL, body)
+                .then(data => {
+                    cleaningAndClosingForm(form, errorContainer);
+
+                    console.log('Успешный ответ:', data);
+
+                    location.reload()
+                })
+                .catch(err => {
+                    displayErrors(err, errorContainer);
+
+                    console.log(err);
+                });
+        });
+    }
+
+    function sendRequest(method, url, body = null) {
+        const headers = {
+            'Content-Type': 'application/json'
+        }
+        return fetch(url, {
+            method: method,
+            body: JSON.stringify(body),
+            headers: headers
+        }).then(response => {
+            if (!response.ok) {
+                return response.json().then(errorData => {
+                    throw errorData;
+                });
+            }
+            return response.json();
+        });
+    }
+    function displayErrors(errors, errorContainer) {
+
+        errorContainer.innerHTML = '';
+        errors.forEach(error => {
+            const errorMessage = document.createElement('div');
+            errorMessage.classList.add('error');
+            errorMessage.textContent = error;
+            errorContainer.appendChild(errorMessage);
+        });
+    }
+
+    function cleaningAndClosingForm(form, errorContainer) {
+
+        errorContainer.innerHTML = '';
+        for (const key in form) {
+            if (form.hasOwnProperty(key)) {
+                form[key].value = '';
+            }
+        }
+        hiddenOpen_Closeclick();
+    }
+    
+});
